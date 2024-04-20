aa226dd2bae54d5ce4a39c4f6d2770c26e1592a9
bikesh2023 <bchaudhary@eeposit.com>
2024-04-20 16:23:46 +0545

    fix: login and logtout

--- js/navbar.js
+++ js/navbar.js
@@ -5,28 +5,33 @@ window.addEventListener("DOMContentLoaded", () => {
       document.getElementById("navbarContainer").innerHTML = html;
 
       const userData = localStorage.getItem("userData");
-      const token = JSON.parse(userData).data.token;
+      const token = JSON.parse(userData)?.data?.token;
       console.log("🚀 ~ .then ~ token:", token);
+
       const logoutButton = document.getElementById("logout-btn");
       const logoutLi = document.getElementById("logout-li");
       const userNameLink = document.getElementById("userNameLink");
-      const userName = JSON.parse(userData).data.fullName;
+      const userName = JSON.parse(userData)?.data?.fullName;
       const isSuccess = JSON.parse(userData)?.data?.success;
-      document.getElementById("userNameLink").innerText = userName;
+
+      // Update the text content of the userNameLink
+      userNameLink.innerText = userName;
+
       logoutButton.addEventListener("click", () => {
         localStorage.clear();
         window.location.replace("feed.html");
       });
 
+      // Check if the user is logged in and update UI accordingly
       if (Boolean(token)) {
         logoutLi.classList.remove("d-none");
         userNameLink.classList.remove("d-none");
-        document
-          .getElementById("login-btn")
-          .parentNode.parentNode.classList.add("d-none");
+        // Remove login and sign up buttons if user is logged in
+        document.getElementById("login-btn").parentNode.classList.add("d-none");
         document
           .getElementById("register-btn")
-          .parentNode.parentNode.classList.add("d-none");
+          .parentNode.classList.add("d-none");
+        // Show the add post button
         document.getElementById("add-post").classList.remove("d-none");
         logoutButton.classList.remove("d-none");
       } else {
