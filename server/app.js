c4d7833c711364c42e4176db52d7b1701bc3d133
bikesh2023 <bchaudhary@eeposit.com>
2024-05-22 16:54:21 +0545

    fix: app.js else if

--- server/app.js
+++ server/app.js
@@ -30,6 +30,82 @@ const onRequest = async (req, res) => {
   req.on("end", async () => {
     req.body = qs.parse(body);
 
+    // if (method === "OPTIONS") {
+    //   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    //   res.setHeader(
+    //     "Access-Control-Allow-Headers",
+    //     "Content-Type, Content-Range, Content-Disposition, Content-Description"
+    //   );
+    //   res.end();
+    // } else {
+    //   if (method === "GET" && req.url === "/") {
+    //     res.setHeader("Content-Type", "text/html");
+    //     const filePath = paths.join(__dirname, "./public/template/index.html");
+    //     try {
+    //       const htmlContent = fs.readFileSync(filePath, "utf8");
+    //       res.writeHead(200);
+    //       res.end(htmlContent);
+    //     } catch (error) {
+    //       console.error("Error reading HTML file:", error);
+    //       res.writeHead(500);
+    //       res.end("Internal Server Error");
+    //     }
+    //   }
+
+    //   if (method === "GET" && req.url === "/users") {
+    //     res.writeHead(200, { "Content-Type": "application/json" });
+    //     await getUsers(req, res);
+    //   }
+
+    //   if (method === "POST" && req.url === "/login") {
+    //     res.writeHead(200, { "Content-Type": "application/json" });
+    //     await loginUser(req, res);
+    //   }
+
+    //   if (method === "POST" && req.url === "/register") {
+    //     res.writeHead(200, { "Content-Type": "application/json" });
+    //     await registerUser(req, res);
+    //   }
+
+    //   if (method === "POST" && req.url === "/feeds") {
+    //     if (req.url === "/online-class") {
+    //       res.writeHead(200, { "Content-Type": "application/json" });
+    //       await addOnlineClass(req, res);
+    //     } else {
+    //       await addNewsFeed(req, res);
+    //     }
+    //   }
+
+    //   if (method === "GET" && req.url === "/feeds") {
+    //     res.writeHead(200, { "Content-Type": "application/json" });
+    //     await getFeeds(req, res);
+    //   }
+
+    //   if (method === "GET" && req.url === "/online-class") {
+    //     res.writeHead(200, { "Content-Type": "application/json" });
+    //     await getOnlineClass(req, res);
+    //   }
+
+    //   if (method === "GET" && req.url === "/tutor") {
+    //     res.writeHead(200, { "Content-Type": "application/json" });
+    //     await getTutor(req, res);
+    //   }
+
+    //   if (
+    //     !(method === "GET" && req.url === "/") &&
+    //     !(method === "GET" && req.url === "/users") &&
+    //     !(method === "POST" && req.url === "/login") &&
+    //     !(method === "POST" && req.url === "/register") &&
+    //     !(method === "POST" && req.url === "/feeds") &&
+    //     !(method === "GET" && req.url === "/feeds") &&
+    //     !(method === "GET" && req.url === "/online-class") &&
+    //     !(method === "GET" && req.url === "/tutor")
+    //   ) {
+    //     res.writeHead(404);
+    //     res.end(JSON.stringify({ error: "Not Found" }));
+    //   }
+    // }
+
     if (method === "OPTIONS") {
       res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
       res.setHeader(
@@ -37,73 +113,39 @@ const onRequest = async (req, res) => {
         "Content-Type, Content-Range, Content-Disposition, Content-Description"
       );
       res.end();
+    } else if (method === "GET" && req.url === "/") {
+      res.setHeader("Content-Type", "text/html");
+      const filePath = paths.join(__dirname, "./public/template/index.html");
+      try {
+        const htmlContent = fs.readFileSync(filePath, "utf8");
+        res.writeHead(200);
+        res.end(htmlContent);
+      } catch (error) {
+        console.error("Error reading HTML file:", error);
+        res.writeHead(500);
+        res.end("Internal Server Error");
+      }
+    } else if (method === "GET" && req.url === "/users") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      await getUsers(req, res);
+    } else if (method === "POST" && req.url === "/login") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      await loginUser(req, res);
+    } else if (method === "POST" && req.url === "/register") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      await registerUser(req, res);
+    } else if (method === "POST" && req.url === "/feeds") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      await addNewsFeed(req, res);
+    } else if (method === "GET" && req.url === "/feeds") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      await getFeeds(req, res);
+    } else if (method === "GET" && req.url === "/tutor") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      await getTutor(req, res);
     } else {
-      if (method === "GET" && req.url === "/") {
-        res.setHeader("Content-Type", "text/html");
-        const filePath = paths.join(__dirname, "./public/template/index.html");
-        try {
-          const htmlContent = fs.readFileSync(filePath, "utf8");
-          res.writeHead(200);
-          res.end(htmlContent);
-        } catch (error) {
-          console.error("Error reading HTML file:", error);
-          res.writeHead(500);
-          res.end("Internal Server Error");
-        }
-      }
-
-      if (method === "GET" && req.url === "/users") {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        await getUsers(req, res);
-      }
-
-      if (method === "POST" && req.url === "/login") {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        await loginUser(req, res);
-      }
-
-      if (method === "POST" && req.url === "/register") {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        await registerUser(req, res);
-      }
-
-      if (method === "POST" && req.url === "/feeds") {
-        if (req.url === "/online-class") {
-          res.writeHead(200, { "Content-Type": "application/json" });
-          await addOnlineClass(req, res);
-        } else {
-          await addNewsFeed(req, res);
-        }
-      }
-
-      if (method === "GET" && req.url === "/feeds") {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        await getFeeds(req, res);
-      }
-
-      if (method === "GET" && req.url === "/online-class") {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        await getOnlineClass(req, res);
-      }
-
-      if (method === "GET" && req.url === "/tutor") {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        await getTutor(req, res);
-      }
-
-      if (
-        !(method === "GET" && req.url === "/") &&
-        !(method === "GET" && req.url === "/users") &&
-        !(method === "POST" && req.url === "/login") &&
-        !(method === "POST" && req.url === "/register") &&
-        !(method === "POST" && req.url === "/feeds") &&
-        !(method === "GET" && req.url === "/feeds") &&
-        !(method === "GET" && req.url === "/online-class") &&
-        !(method === "GET" && req.url === "/tutor")
-      ) {
-        res.writeHead(404);
-        res.end(JSON.stringify({ error: "Not Found" }));
-      }
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: "Not Found" }));
     }
   });
 };
